ba8620fc6a0650d60ff5a58f2d0b1f0b9b945e89
Vladislav Zhukov <officialzhukov@gmail.com>
2023-11-21 18:28:07 +0300

    created project

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div className="text-green-400">Росточек - Интернет-магазин овощей и растений</div>
+    </>
+  )
+}
+
+export default App
